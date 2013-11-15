23a84a5adc09fb448f5cf3eaeb178c84f2c1a6af
Tikue Anazodo <antikceo@gmail.com>
2013-11-15 03:48:41 -0500

    adding html

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+/*
 var http = require('http');
-http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Hello World\n');
-}).listen(process.env.PORT);
+
+http.createServer(
+    function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World\n');
+    }
+).listen(process.env.PORT);
+*/
+
+var connect = require('connect');
+
+connect.createServer(
+    connect.static(__dirname)
+).listen(process.env.PORT);
