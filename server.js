a653724a4b409f60caac9b6c858c5417ab8c4994
Tikue Anazodo <antikceo@gmail.com>
2013-11-16 02:25:10 -0500

    modified html

--- server.js
+++ server.js
@@ -1,30 +1,3 @@
-/*
-var http = require('http');
-
-http.createServer(
-    function (req, res) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello World\n');
-    }
-).listen(process.env.PORT);
-
-
-var connect = require('connect');
-
-connect.createServer(
-    connect.static(__dirname)
-).listen(process.env.PORT);
-
-
-
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname));
-
-app.listen(process.env.PORT);
-
-*/
 
 var express = require('express'),
     app = express();
