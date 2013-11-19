7e38dc07c3f127a5c48932a0520efdeb31cc7d93
Tikue Anazodo <antikceo@gmail.com>
2013-11-19 03:58:26 -0500

    ...

--- js/speech.js
+++ js/speech.js
@@ -1,120 +1,249 @@
 
-                     
-var working, speech;
+var langs =
+[['Afrikaans',       ['af-ZA']],
+ ['Bahasa Indonesia',['id-ID']],
+ ['Bahasa Melayu',   ['ms-MY']],
+ ['Català',          ['ca-ES']],
+ ['Čeština',         ['cs-CZ']],
+ ['Deutsch',         ['de-DE']],
+ ['English',         ['en-AU', 'Australia'],
+                     ['en-CA', 'Canada'],
+                     ['en-IN', 'India'],
+                     ['en-NZ', 'New Zealand'],
+                     ['en-ZA', 'South Africa'],
+                     ['en-GB', 'United Kingdom'],
+                     ['en-US', 'United States']],
+ ['Español',         ['es-AR', 'Argentina'],
+                     ['es-BO', 'Bolivia'],
+                     ['es-CL', 'Chile'],
+                     ['es-CO', 'Colombia'],
+                     ['es-CR', 'Costa Rica'],
+                     ['es-EC', 'Ecuador'],
+                     ['es-SV', 'El Salvador'],
+                     ['es-ES', 'España'],
+                     ['es-US', 'Estados Unidos'],
+                     ['es-GT', 'Guatemala'],
+                     ['es-HN', 'Honduras'],
+                     ['es-MX', 'México'],
+                     ['es-NI', 'Nicaragua'],
+                     ['es-PA', 'Panamá'],
+                     ['es-PY', 'Paraguay'],
+                     ['es-PE', 'Perú'],
+                     ['es-PR', 'Puerto Rico'],
+                     ['es-DO', 'República Dominicana'],
+                     ['es-UY', 'Uruguay'],
+                     ['es-VE', 'Venezuela']],
+ ['Euskara',         ['eu-ES']],
+ ['Français',        ['fr-FR']],
+ ['Galego',          ['gl-ES']],
+ ['Hrvatski',        ['hr_HR']],
+ ['IsiZulu',         ['zu-ZA']],
+ ['Íslenska',        ['is-IS']],
+ ['Italiano',        ['it-IT', 'Italia'],
+                     ['it-CH', 'Svizzera']],
+ ['Magyar',          ['hu-HU']],
+ ['Nederlands',      ['nl-NL']],
+ ['Norsk bokmål',    ['nb-NO']],
+ ['Polski',          ['pl-PL']],
+ ['Português',       ['pt-BR', 'Brasil'],
+                     ['pt-PT', 'Portugal']],
+ ['Română',          ['ro-RO']],
+ ['Slovenčina',      ['sk-SK']],
+ ['Suomi',           ['fi-FI']],
+ ['Svenska',         ['sv-SE']],
+ ['Türkçe',          ['tr-TR']],
+ ['български',       ['bg-BG']],
+ ['Pусский',         ['ru-RU']],
+ ['Српски',          ['sr-RS']],
+ ['한국어',            ['ko-KR']],
+ ['中文',             ['cmn-Hans-CN', '普通话 (中国大陆)'],
+                     ['cmn-Hans-HK', '普通话 (香港)'],
+                     ['cmn-Hant-TW', '中文 (台灣)'],
+                     ['yue-Hant-HK', '粵語 (香港)']],
+ ['日本語',           ['ja-JP']],
+ ['Lingua latīna',   ['la']]];
 
-if (typeof(webkitSpeechRecognition) !== 'function') {  
-  document.getElementById("labnol").innerHTML = "Sorry but Blabnotes only works in the latest version of Google Chrome.";
-  document.getElementById("messages").style.display = "none";
+for (var i = 0; i < langs.length; i++) {
+  select_language.options[i] = new Option(langs[i][0], i);
+}
+select_language.selectedIndex = 6;
+updateCountry();
+select_dialect.selectedIndex = 6;
+document.getElementById("info").innerHTML = "Click To Begin Dictation.";
+
+function updateCountry() {
+  for (var i = select_dialect.options.length - 1; i >= 0; i--) {
+    select_dialect.remove(i);
+  }
+  var list = langs[select_language.selectedIndex];
+  for (var i = 1; i < list.length; i++) {
+    select_dialect.options.add(new Option(list[i][1], list[i][0]));
+  }
+  select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
+}
+
+var create_email = false;
+var final_transcript = '';
+var recognizing = false;
+var ignore_onend;
+var start_timestamp;
+if (!('webkitSpeechRecognition' in window)) {
+  upgrade();
 } else {
+  start_button.style.display = 'inline-block';
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
 
-  speech = new webkitSpeechRecognition();
-  speech.continuous = true;
-  speech.maxAlternatives = 5;
-  speech.interimResults = true;
-
-  speech.lang = window.navigator.userLanguage || window.navigator.language;
-  speech.onend = reset;
- 
-  reset();
-  
-  speech.onerror = function (e) {
-    var msg = e.error + " error"; 
-    if ( e.error === 'no-speech' ) {
-      msg = "No speech was detected. Please try again."; 
-    } else if ( e.error === 'audio-capture' ) {
-      msg = "Please ensure that a microphone is connected to your computer."; 
-    } else if ( e.error === 'not-allowed' ) {
-      msg = "Blabnotes cannot access your microphone. Please go to chrome://settings/contentExceptions#media-stream and allow Microphone access to this website."; 
-    } 
-    document.getElementById("warning").innerHTML = "<p>" + msg + "</p>"; 
-    setTimeout(function() {
-      document.getElementById("warning").innerHTML = ""; 
-    }, 5000);
+  recognition.onstart = function() {
+    recognizing = true;
+    document.getElementById("info").innerHTML = "Speak Now.";
+    start_img.src = 'mic-animate.gif';
   };
-  
-  speech.onresult = function (e) {
-    for (var i = e.resultIndex; i < e.results.length; ++i) {
-      if (e.results[i].isFinal) {
 
-        var words = document.getElementById("labnol"); 
-        var val = e.results[i][0].transcript; 
-
-        if (val === " new paragraph") {
-          val = ".<br><br> "; 
-        } else if (val === " new sentence") {
-          val = ". ";
-        } else if (val === " stop listening") {
-          val = ". "; action();
-        } else if (val === " insert  pause") {
-          val = "? "; action();
-        } else if (val === " insert exclamation") {
-          val = "! "; action();
-        } 
-        
-        if (words.innerHTML.substr(-2) === ". ") {
-          val = val.substr(1,1).toUpperCase() + val.substr(2);          
-        }        
-
-        if (words.innerHTML.length === 0) {
-          val = val.substr(0,1).toUpperCase() + val.substr(1);          
-        }        
-        
-        document.getElementById("notfinal").innerHTML = ""; 
-        words.innerHTML += val;        
-      } else {        
-        document.getElementById("notfinal").innerHTML = e.results[i][0].transcript;
+  recognition.onerror = function(event) {
+    if (event.error == 'no-speech') {
+      start_img.src = 'mic.gif';
+      document.getElementById("info").innerHTML = "No sound was detected. You may need to adjust your <a href='//support.google.com/chrome/bin/answer.py?hl=en&amp;answer=1407892'> microphone settings</a>";
+      ignore_onend = true;
+    }
+    if (event.error == 'audio-capture') {
+      start_img.src = 'mic.gif';
+      document.getElementById("info").innerHTML = "No microphone was found. Ensure that a microphone is installed and that <a href='//support.google.com/chrome/bin/answer.py?hl=en&amp;answer=1407892'> microphone settings</a> are configured correctly.";
+      ignore_onend = true;
+    }
+    if (event.error == 'not-allowed') {
+      if (event.timeStamp - start_timestamp < 100) {
+        document.getElementById("info").innerHTML = "Click the 'Allow' button above to enable your microphone.";
+      } else {
+        document.getElementById("info").innerHTML = "Permission to use microphone is blocked. To change, go to chrome://settings/contentExceptions#media-stream";
       }
+      ignore_onend = true;
     }
   };
-  
-  if (typeof(localStorage) !== 'undefined' ) {
-    if ( localStorage.narration === 'undefined' ) {
-      localStorage.narration = ""; 
+
+  recognition.onend = function() {
+    recognizing = false;
+    if (ignore_onend) {
+      return;
     }
-    
-    document.getElementById("labnol").innerHTML = localStorage.narration;
-  
-    setInterval (function () {
-      var text = document.getElementById("labnol").innerHTML;  
-      if (text !== localStorage.narration) {
-        localStorage.narration = text;        
+    start_img.src = 'mic.gif';
+    if (!final_transcript) {
+      showInfo('info_start');
+      return;
+    }
+    showInfo('');
+    if (window.getSelection) {
+      window.getSelection().removeAllRanges();
+      var range = document.createRange();
+      range.selectNode(document.getElementById('final_span'));
+      window.getSelection().addRange(range);
+    }
+    if (create_email) {
+      create_email = false;
+      createEmail();
+    }
+  };
+
+  recognition.onresult = function(event) {
+    var interim_transcript = '';
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
       }
-    }, 5000);       
-  }  
-}
-  
-function clearSlate() { 
-  if (working) {
-    speech.stop();
-  } 
-  document.getElementById("labnol").innerHTML = "";
-  reset();
+    }
+    final_transcript = capitalize(final_transcript);
+    final_span.innerHTML = linebreak(final_transcript);
+    interim_span.innerHTML = linebreak(interim_transcript);
+    if (final_transcript || interim_transcript) {
+      showButtons('inline-block');
+    }
+  };
 }
 
-function reset() {
-  working = false; 
-  document.getElementById("status").style.display="none"; 
-  document.getElementById("btn").innerHTML = "Begin Transcription";  
+function upgrade() {
+  start_button.style.visibility = 'hidden';
+  showInfo('info_upgrade');
 }
 
-function action() {
-  if (working) {
-    speech.stop(); 
-    reset(); 
+var two_line = /\n\n/g;
+var one_line = /\n/g;
+function linebreak(s) {
+  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+
+var first_char = /\S/;
+function capitalize(s) {
+  return s.replace(first_char, function(m) { return m.toUpperCase(); });
+}
+
+function createEmail() {
+  var n = final_transcript.indexOf('\n');
+  if (n < 0 || n >= 80) {
+    n = 40 + final_transcript.substring(40).indexOf(' ');
+  }
+  var subject = encodeURI(final_transcript.substring(0, n));
+  var body = encodeURI(final_transcript.substring(n + 1));
+  window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
+}
+
+function copyButton() {
+  if (recognizing) {
+    recognizing = false;
+    recognition.stop();
+  }
+  copy_button.style.display = 'none';
+  copy_info.style.display = 'inline-block';
+  showInfo('');
+}
+
+function emailButton() {
+  if (recognizing) {
+    create_email = true;
+    recognizing = false;
+    recognition.stop();
   } else {
-    speech.start(); 
-    working = true; 
-    document.getElementById("btn").innerHTML = "End Transcription"; 
-  } 
+    createEmail();
+  }
+  email_button.style.display = 'none';
+  email_info.style.display = 'inline-block';
+  showInfo('');
 }
 
-    function toggleVisibility(selectedTab) {
-         var content = document.getElementsByClassName('info');
-         for(var i=0; i<content.length; i++) {
-              if(content[i].id == selectedTab) {
-                    content[i].style.display = 'block';
-              } else {
-                    content[i].style.display = 'none';
-              }
-         }
-    }
+function startButton(event) {
+  if (recognizing) {
+    recognition.stop();
+    return;
+  }
+  final_transcript = '';
+  recognition.lang = select_dialect.value;
+  recognition.start();
+  ignore_onend = false;
+  //final_span.innerHTML = '';
+  interim_span.innerHTML = '';
+  start_img.src = 'mic-slash.gif';
+  showInfo('info_allow');
+  showButtons('none');
+  start_timestamp = event.timeStamp;
+}
+
+function stopButton(event) {
+  if (recognizing) {
+    recognition.stop();
+    return;
+  }
+}
+
+var current_style;
+function showButtons(style) {
+  if (style == current_style) {
+    return;
+  }
+  current_style = style;
+  copy_button.style.display = style;
+  email_button.style.display = style;
+  copy_info.style.display = 'none';
+  email_info.style.display = 'none';
+}        
